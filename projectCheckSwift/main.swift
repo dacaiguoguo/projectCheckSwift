1d3b43228fc8e760a051e3ed57577b720d946928
dacaiguoguo <dacaiguoguo@gmail.com>
2016-03-14 13:26:40 +0800

    Initial Commit

---
+++ projectCheckSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  projectCheckSwift
+//
+//  Created by yanguo sun on 16/3/14.
+//  Copyright © 2016年 Lvmama. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
